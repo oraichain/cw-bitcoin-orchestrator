6074fac72d6ea06e10be691bb3758130d6c86759
perfogic <longdieu12x@gmail.com>
2024-08-27 01:54:44 +0700

    fix: fix api contract config

--- packages/orchestrator/src/apis/controllers/contract.controller.ts
+++ packages/orchestrator/src/apis/controllers/contract.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
-import contractService from "../services/bitcoin.service";
+import contractService from "../services/contract.service";
 
 const getConfig = catchAsync(async (req: Request, res: Response) => {
   const data = await contractService.getConfig();
   res.status(httpStatus.OK).json({
-    message: "Get bitcoin config successfully",
+    message: "Get contract config successfully",
     data,
   });
 });
