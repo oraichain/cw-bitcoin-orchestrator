3266ff696b8a9219cf07ad14318227b0468e6653
perfogic <longdieu12x@gmail.com>
2024-10-04 01:03:47 +0700

    update morgan

--- packages/orchestrator/src/configs/morgan.ts
+++ packages/orchestrator/src/configs/morgan.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import morgan from "morgan";
-import logger from "./logger";
+import { logger } from "./logger";
 
+const morganLogger = logger("morgan");
 morgan.token(
   "message",
   (req: Request, res: Response) =>
@@ -18,12 +19,12 @@ const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-ti
 
 const successHandler = morgan(successResponseFormat, {
   skip: (req, res) => res.statusCode >= 400,
-  stream: { write: (message) => logger.info(message.trim()) },
+  stream: { write: (message) => morganLogger.info(message.trim()) },
 });
 
 const errorHandler = morgan(errorResponseFormat, {
   skip: (req, res) => res.statusCode < 400,
-  stream: { write: (message) => logger.error(message.trim()) },
+  stream: { write: (message) => morganLogger.error(message.trim()) },
 });
 
 export default {
