e6031810e61428e7ece99096b3dde6a7e73150b6
perfogic <longdieu12x@gmail.com>
2024-10-15 10:06:36 +0700

    update stats memwatch

--- packages/orchestrator/src/bin/relayer.ts
+++ packages/orchestrator/src/bin/relayer.ts
@@ -1,25 +1,29 @@
-import { AppBitcoinClient, LightClientBitcoinClient } from '@oraichain/bitcoin-bridge-contracts-sdk';
-import { RPCClient } from '@oraichain/rpc-bitcoin';
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import express from 'express';
-import helmet from 'helmet';
-import http from 'http';
-import httpStatus from 'http-status';
-import os from 'os';
-import path from 'path';
-import xss from 'xss-clean';
-import bitcoinRoute from '../apis/routes/bitcoin.route';
-import checkpointRoute from '../apis/routes/checkpoint.route';
-import contractRoute from '../apis/routes/contract.route';
-import env from '../configs/env';
-import morgan from '../configs/morgan';
-import { OraichainConfig } from '../configs/networks';
-import { DuckDbNode } from '../services/db';
-import RelayerService from '../services/relayer';
-import { initSignerClient } from '../utils/cosmos';
-import { decryptMnemonic } from '../utils/mnemonic';
+import memwatch from "@airbnb/node-memwatch";
+import {
+  AppBitcoinClient,
+  LightClientBitcoinClient,
+} from "@oraichain/bitcoin-bridge-contracts-sdk";
+import { RPCClient } from "@oraichain/rpc-bitcoin";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import http from "http";
+import httpStatus from "http-status";
+import os from "os";
+import path from "path";
+import xss from "xss-clean";
+import bitcoinRoute from "../apis/routes/bitcoin.route";
+import checkpointRoute from "../apis/routes/checkpoint.route";
+import contractRoute from "../apis/routes/contract.route";
+import env from "../configs/env";
+import morgan from "../configs/morgan";
+import { OraichainConfig } from "../configs/networks";
+import { DuckDbNode } from "../services/db";
+import RelayerService from "../services/relayer";
+import { initSignerClient } from "../utils/cosmos";
+import { decryptMnemonic } from "../utils/mnemonic";
 
 const start = async () => {
   const app = express();
@@ -30,7 +34,7 @@ const start = async () => {
   app.use(helmet());
   app.use(
     cors({
-      origin: '*'
+      origin: "*",
     })
   );
   app.use(express.json());
@@ -39,24 +43,24 @@ const start = async () => {
   app.use(cookieParser());
   app.use((err: any, req: any, res: any, next: any) => {
     const status = err.status || 500;
-    const message = err.message || 'Something went wrong';
+    const message = err.message || "Something went wrong";
     return res.status(status).json({
       status,
       message,
       success: false,
-      stack: env.server.env == 'development' ? err.stack : null
+      stack: env.server.env == "development" ? err.stack : null,
     });
   });
 
   const PORT = env.server.port;
 
-  app.use('/api/bitcoin', bitcoinRoute);
-  app.use('/api/checkpoint', checkpointRoute);
-  app.use('/api/contract', contractRoute);
-  app.get('/node_info', (_, res) => {
+  app.use("/api/bitcoin", bitcoinRoute);
+  app.use("/api/checkpoint", checkpointRoute);
+  app.use("/api/contract", contractRoute);
+  app.get("/node_info", (_, res) => {
     res.status(httpStatus.OK).json({
-      message: 'Ok',
-      data: []
+      message: "Ok",
+      data: [],
     });
   });
 
@@ -64,33 +68,61 @@ const start = async () => {
     const homeDir = os.homedir();
     const relayerDirPath = path.join(homeDir, env.server.storageDirName);
 
-    console.log('[ACTIVE] Server is running on port ' + PORT);
-    console.log('Orchestrator is storing data at ' + relayerDirPath);
-    console.log('Initilizing DuckDB...');
+    console.log("[ACTIVE] Server is running on port " + PORT);
+    console.log("Orchestrator is storing data at " + relayerDirPath);
+    console.log("Initilizing DuckDB...");
     await DuckDbNode.create(env.duckdb.name);
-    console.log('Initilized DuckDB!');
-    console.log('Creating tables in DuckDB!');
+    console.log("Initilized DuckDB!");
+    console.log("Creating tables in DuckDB!");
     await DuckDbNode.instances.createTable();
-    console.log('Tables are created in DuckDB!');
+    console.log("Tables are created in DuckDB!");
 
     let mnemonic = env.cosmos.mnemonic;
     if (!mnemonic) {
-      mnemonic = decryptMnemonic('Mnemonic passphrase:', env.cosmos.encryptedMnemonic);
+      mnemonic = decryptMnemonic(
+        "Mnemonic passphrase:",
+        env.cosmos.encryptedMnemonic
+      );
     }
+
+    memwatch.on("stats", function (info) {
+      // Take first snapshot
+      var hd = new memwatch.HeapDiff();
+      var diff = hd.end();
+      console.log("Differ:", JSON.stringify(diff));
+    });
 
     const btcClient = new RPCClient({
       port: env.bitcoin.port,
       host: env.bitcoin.host,
       user: env.bitcoin.username,
-      pass: env.bitcoin.password
+      pass: env.bitcoin.password,
     });
 
     const { prefix, gasPrice } = OraichainConfig;
 
-    const { sender, client } = await initSignerClient(env.cosmos.rpcUrl, mnemonic, prefix, gasPrice);
-    const lightClientBitcoinClient = new LightClientBitcoinClient(client, sender, env.cosmos.lightClientBitcoin);
-    const appBitcoinClient = new AppBitcoinClient(client, sender, env.cosmos.appBitcoin);
-    const relayerService = new RelayerService(btcClient, lightClientBitcoinClient, appBitcoinClient, DuckDbNode.instances);
+    const { sender, client } = await initSignerClient(
+      env.cosmos.rpcUrl,
+      mnemonic,
+      prefix,
+      gasPrice
+    );
+    const lightClientBitcoinClient = new LightClientBitcoinClient(
+      client,
+      sender,
+      env.cosmos.lightClientBitcoin
+    );
+    const appBitcoinClient = new AppBitcoinClient(
+      client,
+      sender,
+      env.cosmos.appBitcoin
+    );
+    const relayerService = new RelayerService(
+      btcClient,
+      lightClientBitcoinClient,
+      appBitcoinClient,
+      DuckDbNode.instances
+    );
     RelayerService.instances = relayerService;
 
     await Promise.all([relayerService.relay()]);
