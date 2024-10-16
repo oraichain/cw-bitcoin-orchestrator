89c7d40c6301bf6910aeb0ee91d6595c3f95e9e1
perfogic <longdieu12x@gmail.com>
2024-10-16 15:57:32 +0700

    update code

--- packages/orchestrator/src/services/block_header/index.ts
+++ packages/orchestrator/src/services/block_header/index.ts
@@ -1,9 +1,9 @@
-import { RPCClient } from '@oraichain/rpc-bitcoin';
-import { VerbosedBlockHeader } from 'src/@types';
-import { Logger } from 'winston';
-import { logger } from '../../configs/logger';
-import { TableName } from '../../utils/db';
-import { DuckDbNode } from '../db';
+import { RPCClient } from "@oraichain/rpc-bitcoin";
+import { VerbosedBlockHeader } from "src/@types";
+import { Logger } from "winston";
+import { logger } from "../../configs/logger";
+import { TableName } from "../../utils/db";
+import { DuckDbNode } from "../db";
 
 export interface BlockHeaderInterface {
   hash: string;
@@ -14,31 +14,39 @@ class BlockHeaderService {
   logger: Logger;
 
   constructor(protected btcClient: RPCClient, protected db: DuckDbNode) {
-    this.logger = logger('BlockHeaderService');
+    this.logger = logger("BlockHeaderService");
   }
 
   async insert(blockhash: string): Promise<VerbosedBlockHeader> {
     try {
-      const blockHeaderData: VerbosedBlockHeader = await this.btcClient.getblockheader({
-        blockhash,
-        verbose: true
-      });
+      const blockHeaderData: VerbosedBlockHeader =
+        await this.btcClient.getblockheader({
+          blockhash,
+          verbose: true,
+        });
       await this.db.insert(TableName.BlockHeader, {
         hash: blockhash,
-        data: JSON.stringify(blockHeaderData)
+        data: JSON.stringify(blockHeaderData),
       });
       this.logger.info(`Inserted new block header with hash ${blockhash}`);
       return blockHeaderData;
     } catch (err) {
-      this.logger.error(`Error inserting block header with hash ${blockhash}`, err);
+      this.logger.error(
+        `Error inserting block header with hash ${blockhash}`,
+        err
+      );
       throw err;
     }
   }
 
   async getBlockHeader(blockhash: string): Promise<VerbosedBlockHeader | null> {
-    const data: BlockHeaderInterface[] = await this.db.select(TableName.BlockHeader, {
-      where: { hash: blockhash }
-    });
+    const data: BlockHeaderInterface[] = await this.db.select(
+      TableName.BlockHeader,
+      {
+        where: { hash: blockhash },
+      }
+    );
+    console.log({ data });
     if (data.length === 0) {
       return this.insert(blockhash);
     }
