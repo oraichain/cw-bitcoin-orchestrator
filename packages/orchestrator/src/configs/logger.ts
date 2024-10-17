0b8c387a0fbcf34e04433df58d765a9ef40c06cc
perfogic <longdieu12x@gmail.com>
2024-10-17 15:25:26 +0700

    update logger

--- packages/orchestrator/src/configs/logger.ts
+++ packages/orchestrator/src/configs/logger.ts
@@ -2,9 +2,11 @@ export const logger = (label: string, loglevel?: string): any => {
   return {
     info: (text: string, ...args) =>
       console.info(`${new Date().toLocaleString()} [INFO] [${label}]: ${text}`),
-    error: (text: string, ...args) =>
+    error: (text: string, ...args) => {
+      console.log(args);
       console.error(
         `${new Date().toLocaleString()} [ERROR] [${label}]: ${text}, ${args}`
-      ),
+      );
+    },
   };
 };
