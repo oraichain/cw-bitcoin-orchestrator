7a7fb4fb06d5ad73470f9ad242883f1629153323
perfogic <longdieu12x@gmail.com>
2024-10-17 18:01:42 +0700

    update logger

--- packages/orchestrator/src/configs/logger.ts
+++ packages/orchestrator/src/configs/logger.ts
@@ -1,11 +1,26 @@
+import { EmbedBuilder, WebhookClient } from "discord.js";
+import env from "./env";
+
 export const logger = (label: string, loglevel?: string): any => {
   return {
-    info: (text: string, ...args) =>
+    info: (text: string) =>
       console.info(`${new Date().toLocaleString()} [INFO] [${label}]: ${text}`),
-    error: (text: string, ...args) => {
-      console.log(args);
+    error: (text: string, error: Error) => {
+      const webhookClient = new WebhookClient({
+        url: env.logger.webhookUrl as string,
+      });
+      const embed = new EmbedBuilder()
+        .setTitle(text)
+        .setDescription(error.stack)
+        .setColor("Red")
+        .setImage(
+          "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*PnNRGFIy2mTXtIJzNAoy_A.jpeg"
+        );
+      webhookClient.send({
+        embeds: [embed],
+      });
       console.error(
-        `${new Date().toLocaleString()} [ERROR] [${label}]: ${text}, ${args}`
+        `${new Date().toLocaleString()} [ERROR] [${label}]: ${text}, ${error}`
       );
     },
   };
