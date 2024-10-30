dcf1df8d38029a5be81eb1c28f950727217ea467
Pham Tu <tubackkhoa@gmail.com>
2024-10-30 10:02:08 +0700

    using singleton webhookClient

--- packages/orchestrator/src/configs/logger.ts
+++ packages/orchestrator/src/configs/logger.ts
@@ -1,29 +1,23 @@
-import { EmbedBuilder, WebhookClient } from "discord.js";
-import env from "./env";
+import { MessageEmbed, WebhookClient } from 'discord.js';
+import env from './env';
+
+const webhookClient = new WebhookClient({
+  url: env.logger.webhookUrl as string
+});
 
 export const logger = (label: string, loglevel?: string): any => {
   return {
-    info: (text: string) =>
-      console.info(`${new Date().toLocaleString()} [INFO] [${label}]: ${text}`),
+    info: (text: string) => console.info(`${new Date().toLocaleString()} [INFO] [${label}]: ${text}`),
     error: (text: string, error: Error) => {
-      const webhookClient = new WebhookClient({
-        url: env.logger.webhookUrl as string,
-      });
-      const embed = new EmbedBuilder()
+      const embed = new MessageEmbed()
         .setTitle(text)
-        .setDescription(
-          error?.stack || error?.message || "Something went wrong!"
-        )
-        .setColor("Red")
-        .setImage(
-          "https://cdn.discordapp.com/attachments/1263547562120577058/1296729095975211050/demo.jpg?ex=67135894&is=67120714&hm=6020d73a3063554bd1eca81e81b3c378e3b84196d577c087d7311d53e6ee009a&"
-        );
+        .setDescription(error?.stack || error?.message || 'Something went wrong!')
+        .setColor('#0099ff')
+        .setImage('https://cdn.discordapp.com/attachments/1263547562120577058/1296729095975211050/demo.jpg?ex=67135894&is=67120714&hm=6020d73a3063554bd1eca81e81b3c378e3b84196d577c087d7311d53e6ee009a&');
       webhookClient.send({
-        embeds: [embed],
+        embeds: [embed]
       });
-      console.error(
-        `${new Date().toLocaleString()} [ERROR] [${label}]: ${text}, ${error}`
-      );
-    },
+      console.error(`${new Date().toLocaleString()} [ERROR] [${label}]: ${text}, ${error}`);
+    }
   };
 };
